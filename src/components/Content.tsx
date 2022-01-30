ee92f36142dcbe0fb860cc00fcc940caa4a62b29
Gamadv <lealdev.py@gmail.com>
2022-01-30 17:30:06 -0300

    ft: movies content component created

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,64 @@
-export function Content() {
+import { useEffect, useState } from 'react';
+import { Header } from '../components/Header';
+import { MovieCard } from './MovieCard';
+
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+
+type MovieProps = {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Runtime: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+};
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+type ContentProps = {
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+};
+
+export function Content(props: ContentProps) {
   // Complete aqui
+  const { selectedGenreId, selectedGenre } = props;
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    if (!selectedGenreId) return;
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
+  }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+      <Header title={selectedGenre.title} />
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
